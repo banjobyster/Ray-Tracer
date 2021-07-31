89b70766af4eef971b4a81fe3510f95e6828deca
banjobyster <77842809+banjobyster@users.noreply.github.com>
2021-07-31 19:33:38 +0530

    Update ray_tracer_sdl.cpp

--- ray_tracer_sdl.cpp
+++ ray_tracer_sdl.cpp
@@ -5,7 +5,7 @@ Ray tracer created with C++ and SDL2
 This ray tracer uses the path tracing technique to send many rays for each pixel and taking the average to give photo-realistic results. It has a sphere class and three material classes - metallic, diffused and dielectric. Further shapes and materials are easy to add due to the object oriented nature of this project.
 
 It also has a camera which can be moved by :
-WASD to move in camera's horizontal plane
+WASD to move in x-z plane
 Spacebar and Left-Ctrl to move in the y axis
 NUMPAD 8 and 2 to rotate vertically
 NUMPAD 4 and 6 to rotate horizontally
